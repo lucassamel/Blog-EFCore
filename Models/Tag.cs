86c44bc33ecb334fedd9ed9374b974c667471e80
Lucas Samel <lucas.samel97@gmail.com>
2024-09-30 20:35:05 -0300

    Create Tag

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Blog.Models
 {
+    [Table("Tag")]
     public class Tag
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new BlogDataContext())
+            {
+                var tag = new Tag { Name = "ASP.NET",
+                Slug= "asp.net"};
+
+                context.Tags.Add(tag);
+                context.SaveChanges();
+            }
+        }
+    }
+}
