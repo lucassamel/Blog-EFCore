36085050c268d7bd0267362c5760936f100a4a72
Lucas Samel <lucas.samel97@gmail.com>
2024-09-30 20:25:38 -0300

    DataContext

--- Models/Category.cs
+++ Models/Category.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Blog.Models
 {
-    public class Catgory
+    public class Category
     {
         public int Id { get; set; }
         public string Name { get; set; }
--- Models/Tag.cs
+++ Models/Tag.cs
@@ -9,13 +9,7 @@ namespace Blog.Models
     public class Tag
     {
         public int Id { get; set; }
-        public int CategoriId { get; set; }
-        public int AuthorId { get; set; }
-        public string Title { get; set; }
-        public string Sumary { get; set; }
-        public string Body { get; set; }
+        public string Name { get; set; }
         public string Slug { get; set; }
-        public DateTime CreateDate { get; set; }
-        public DateTime LastUpdateDate { get; set; }
     }
 }
