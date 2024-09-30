3500c6552c27f36e32ea034c0f6773d0804a13c2
Lucas Samel <lucas.samel97@gmail.com>
2024-09-30 20:17:34 -0300

    Models

---
+++ Models/Tag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+        public int CategoriId { get; set; }
+        public int AuthorId { get; set; }
+        public string Title { get; set; }
+        public string Sumary { get; set; }
+        public string Body { get; set; }
+        public string Slug { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
+    }
+}
