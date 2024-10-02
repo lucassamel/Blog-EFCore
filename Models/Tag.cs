6e10615748dc6ee0b2496638d9483e78f9e33be0
lucassamelFadami <lucas.samel@fadami.com.br>
2024-10-02 09:08:14 -0300

    User Mapping

--- Models/Tag.cs
+++ Models/Tag.cs
@@ -13,5 +13,6 @@ namespace Blog.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
+        public IList<Post> Posts { get; set; }
     }
 }
